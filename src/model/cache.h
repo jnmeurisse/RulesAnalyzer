f7eeb563939ea1d35d375f8f5c718f23f7f282ca
jnmeurisse <88129653+jnmeurisse@users.noreply.github.com>
2026-06-08 10:42:30 +0200

    Upd: cache shared pointer

--- src/model/cache.h
+++ src/model/cache.h
@@ -25,11 +25,11 @@ namespace fwm {
 		Cache(const T&) = delete;
 		Cache& operator=(const T&) = delete;
 
-		const T* get(const std::string& name) const;
-		const T* set(const T* item);
+		std::shared_ptr<const T> get(const std::string& name) const;
+		std::shared_ptr<const T> set(const std::shared_ptr<const T> item);
 
 	private:
-		std::map<const std::string, std::unique_ptr<const T>> _items;
+		std::map<const std::string, std::shared_ptr<const T>> _items;
 	};
 
 
@@ -41,25 +41,29 @@ namespace fwm {
 
 
 	template<typename T>
-	const T* Cache<T>::get(const std::string& name) const
+	std::shared_ptr<const T> Cache<T>::get(const std::string& name) const
 	{
 		const auto it = _items.find(name);
 
 		if (it == _items.end())
 			return nullptr;
 		else
-			return it->second.get();
+			return it->second;
 	}
 
 
 	template<typename T>
-	const T* Cache<T>::set(const T* item)
+	std::shared_ptr<const T> Cache<T>::set(const std::shared_ptr<const T> item)
 	{
+		if (!item)
+			throw std::runtime_error("internal error : null pointer cache::set");
+
 		const std::string& name = item->name();
 
-		// insert the name-value pair into the map or raise an exception if the item is in the cache.
-		const auto it =_items.insert(std::make_pair(name, item));
-		if (!it.second)
+		// insert the name-value pair into the map or raise an exception if the 
+		// item name exists in the cache.
+		const auto result =_items.insert(std::make_pair(name, item));
+		if (!result.second)
 			throw std::runtime_error(fmt::format("internal error : item '{}' already in the cache", name));
 
 		return item;
